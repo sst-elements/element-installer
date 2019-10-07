3268f5b5fa28d2848fbfe8a299d50584ac2ab913
sabbirahm3d <sabbir0ahmed0@gmail.com>
2019-10-07 12:05:19 -0400

    reformat installer into its own repo

---
+++ cli.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+
+import sstelements
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="SST Element Installer")
+    parser.add_argument("--install", "-i", metavar="ELEMENT", type=str, default="",
+                        help="Install element")
+    parser.add_argument("--uninstall", "-u", metavar="ELEMENT", type=str, default="",
+                        help="Uninstall element")
+    parser.add_argument("--force", "-f", action="store_true", default=False,
+                        help="Force installation")
+    parser.add_argument("--list", "-l", action="store_true", default=False,
+                        help="List all elements")
+    parser.add_argument("--registered", "-r", action="store_true", default=False,
+                        help="List registered elements")
+    parser.add_argument("--url", "-x", type=str, default="sabbirahm3d",
+                        help="External URL for element")
+    parser.add_argument("--details", "-d", metavar="ELEMENT", type=str, default="",
+                        help="Display element information")
+
+    args = parser.parse_args().__dict__
+
+    # install and uninstall options are mutually exclusive
+    if args["install"] and args["uninstall"]:
+        parser.print_help()
+        exit(1)
+
+    elif args["install"]:
+        sstelements.install(args["install"], args["url"], args["force"])
+
+    elif args["uninstall"]:
+        sstelements.uninstall(args["uninstall"])
+
+    elif args["list"]:
+        sstelements.pprint_all_elements()
+
+    elif args["registered"]:
+        print("\n".join(sstelements.list_registered_elements()))
+
+    elif args["details"]:
+        print(sstelements.get_info(args["details"]))
+
+    else:
+        parser.print_help()
--- memHierarchy/tests/updateTests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-for i in sdl*.py.new; do
-  target=$(basename $i .new)
-  cp $i ${target}.ref
-done
