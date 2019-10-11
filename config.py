3e4c235b79599aee39531c05c3bbf378356d348d
sabbirahm3d <sabbir0ahmed0@gmail.com>
2019-10-11 12:35:37 -0400

    remove config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# single official list of all trusted elements
-ELEMENT_LIST_URL = "https://raw.githubusercontent.com/sabbirahm3d/sst-elements/installer/elements"
-
-# official repository collection of all elements
-ELEMENT_REPO_URL = "sabbirahm3d"
